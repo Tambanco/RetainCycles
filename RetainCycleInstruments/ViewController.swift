2ae7d8d869ad9f2c02c68ae7e129cbca1a74d342
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-05-12 23:24:25 +0300

    Update ViewController

--- RetainCycleInstruments/ViewController.swift
+++ RetainCycleInstruments/ViewController.swift
@@ -11,9 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Show Red", style: .plain, target: self, action: #selector(handleShowRedController))
     }
-
-
+    
+    @objc func handleShowRedController() {
+        navigationController?.pushViewController(RedController(), animated: true)
+    }
 }
 
+class Service {
+    weak var redCoontroller: RedController?
+}
+
+class RedController: UITableViewController {
+    
+    deinit {
+        print("OS reclaiming memory for RedController")
+    }
+    
+    let service = Service()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = .systemRed
+        
+        service.redCoontroller = self
+    }
+}
